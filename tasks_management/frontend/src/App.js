8da028edda8617a9d6c71e4793e647258a69d9d7
Octavie Ploye <octavieploye@MacBook-Air-de-Octavie.local>
2021-12-07 18:12:40 +0100

    python flushed and frontend added and running

---
+++ tasks_management/frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import axios from 'axios'
+
+function App() {
+  React.useEffect(() => {
+    const getData = async () => {
+      const res = await axios.get('/api/tasks/')
+      console.log(res.data);
+    }
+    getData()
+  })
+  return <h1> Helo World</h1>
+}
+
+export default App
--- tasks_management/tasks_management/urls.py
+++ tasks_management/tasks_management/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tasks/', include('task_app.urls')),
+    path('api/tasks/', include('task_app.urls')),
     # path('api-auth/', include('rest_framework.urls'))
 ]
